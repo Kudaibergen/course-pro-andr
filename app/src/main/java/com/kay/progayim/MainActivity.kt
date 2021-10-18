24af1e2f4eadf7e027ae0866773811a7132cfe6f
Kudaybergen <tuleubekov.git@gmail.com>
2021-10-18 16:50:41 +0600

    first commit

---
+++ app/src/main/java/com/kay/progayim/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.kay.progayim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.txt_hello)
+        textView.text = "asdasda"
+
+    }
+}
