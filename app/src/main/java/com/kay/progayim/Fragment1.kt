24f02f7deae8bf650df42591144789c67e06c12d
Kudaybergen <tuleubekov.git@gmail.com>
2021-12-10 16:19:10 +0600

    class L19

--- app/src/main/java/com/kay/progayim/Fragment1.kt
+++ app/src/main/java/com/kay/progayim/Fragment1.kt
@@ -2,25 +2,25 @@ package com.kay.progayim
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import com.kay.progayim.databinding.Fragment1Binding
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_1.*
 
 class Fragment1 : Fragment(R.layout.fragment_1) {
+    private var _binding: Fragment1Binding? = null
+    private val binding = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = Fragment1Binding.bind(view)
 
-        frgTxt.text = ""
-        btnGo.setOnClickListener {
-
+        binding.apply {
+            frgTxt.text = ""
+            btnToast.setOnClickListener {}
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 }
