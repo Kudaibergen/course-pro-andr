fe403158788ea639275cc597a58dcc6fdfd5cbac
Kudaybergen <tuleubekov.git@gmail.com>
2021-12-21 19:56:41 +0600

    add prepare for binding

--- app/src/main/java/com/kay/progayim/MainActivity.kt
+++ app/src/main/java/com/kay/progayim/MainActivity.kt
@@ -2,12 +2,15 @@ package com.kay.progayim
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kay.progayim.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
