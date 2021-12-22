3ae665022670cd6591fd1e7f32967772806003a2
Kudaybergen <tuleubekov.git@gmail.com>
2021-12-22 17:32:13 +0600

    class

--- app/src/main/java/com/kay/progayim/MainActivity3.kt
+++ app/src/main/java/com/kay/progayim/MainActivity3.kt
@@ -1,6 +1,5 @@
 package com.kay.progayim
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kay.progayim.databinding.ActivityMain3Binding
@@ -13,11 +12,5 @@ class MainActivity3 : AppCompatActivity() {
         binding = ActivityMain3Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn.setOnClickListener {
-            val intent = Intent()
-            intent.putExtra("KEY", "hello from activity 3")
-            setResult(RESULT_OK, intent)
-            finish()
-        }
     }
 }
